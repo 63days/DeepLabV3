764936395b9092fda0373c14669e04fb30c80f99
63days <tretre069@naver.com>
2020-09-02 18:18:17 +0900

    change main.py

--- main.py
+++ main.py
@@ -23,15 +23,19 @@ def main(args):
     model = nn.DataParallel(model).to(device)
 
     optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=len(train_dl), eta_min=0,
+                                                     last_epoch=-1)
     criterion = nn.BCEWithLogitsLoss()
     train_losses = []
     val_losses = []
-    m_IOU_list = []
+    mIOU_list = []
     best_mIOU = 0.
+    step = 0
     for epoch in range(epochs):
-
+        scheduler.step()
         pbar = tqdm(train_dl)
         model.train()
+        losses = []
         for (img, label) in pbar:
             optimizer.zero_grad()
             img, label = img.to(device), label.to(device)
@@ -39,25 +43,53 @@ def main(args):
             loss = criterion(pred, label)
             loss.backward()
             optimizer.step()
+            losses.append(loss)
+            pbar.set_description(f'E: {epoch+1} | L: {loss.item():.4f} | lr: {scheduler.get_lr()[0]:.7f}')
 
-            pbar.set_description(f'E: {epoch+1} | L: {loss.item():.4f}')
-            train_losses.append(loss.item())
+        if (epoch + 1) % 10:
+            losses = sum(losses) / len(losses)
+            train_losses.append(losses)
 
         with torch.no_grad():
             model.eval()
-            m_IOU = []
-            for (img, label) in valid_dl:
+            mIOU = []
+            losses = []
+            pbar = tqdm(valid_dl)
+            for (img, label) in pbar:
                 img, label = img.to(device), label.to(device)
                 pred = model(img)
                 loss = criterion(pred, label)
 
-                m_IOU.append(get_IOU(pred, label, threshold=threshold))
-                val_losses.append(loss.item())
+                mIOU.append(get_IOU(pred, label, threshold=threshold))
+                losses.append(loss.item())
 
-            m_IOU = sum(m_IOU) / len(m_IOU)
-            m_IOU_list.append(m_IOU)
+            mIOU = sum(mIOU) / len(mIOU)
+            mIOU_list.append(mIOU)
+            if (epoch + 1) % 10:
+                losses = sum(losses) / len(losses)
+                val_losses.append(losses)
 
-            print(f'VL: {loss.item():.4f} | mIOU: {100 * m_IOU:.1f}')
+            print(f'VL: {loss.item():.4f} | mIOU: {100 * mIOU:.1f}')
+
+        if mIOU > best_mIOU:
+            best_mIOU = mIOU
+            save_state = {
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'scheduler_state_dict': scheduler.state_dict(),
+                'train_losses': train_losses,
+                'val_losses': val_losses,
+                'best_mIOU': best_mIOU
+            }
+            torch.save(save_state, f'./checkpoint/{100*best_mIOU:.1f}mIOU.ckpt')
+            step = 0
+        else:
+            step += 1
+            if step > args.patience:
+                print('Early stopped...')
+                return
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Semantic Segmentation')
@@ -76,6 +108,11 @@ if __name__ == '__main__':
         '--num_workers',
         type=int,
         default=8
+    )
+    parser.add_argument(
+        '--patience',
+        type=int,
+        default=200
     )
     parser.add_argument(
         '--valid_ratio',
