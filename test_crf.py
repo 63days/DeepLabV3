2fe9988cfd2d618c1190e88e9a44df392e4f4954
63days <tretre069@naver.com>
2020-11-03 15:04:32 +0900

    crf complete

--- test_crf.py
+++ test_crf.py
@@ -8,6 +8,7 @@ from dataloader import DataSetWrapper
 from utils import Padding, get_IOU
 import numpy as np
 import pydensecrf.densecrf as dcrf
+from PIL import Image
 from torchvision import transforms
 from pydensecrf.utils import unary_from_softmax, create_pairwise_bilateral
 
@@ -20,53 +21,51 @@ def main(args):
     up_method = args.up_method
     separable = args.separable
 
-    ds = DataSetWrapper(1, args.num_workers, 0.2)
+    ds = DataSetWrapper(args.batch_size, args.num_workers, 0.2)
     test_dl = ds.get_data_loaders(train=False)
 
     model = Unet(input_dim=1, separable=True,
                  down_method='conv', up_method='transpose')
     model = nn.DataParallel(model).to(device)
 
-    #load_state = torch.load(f'./checkpoint/{down_method}_{up_method}_{separable}.ckpt')
     load_state = torch.load(f'./checkpoint/conv_transpose_True.ckpt')
 
     model.load_state_dict(load_state['model_state_dict'])
-    train_losses = load_state['train_losses']
-    val_losses = load_state['val_losses']
-    map_list = []
+
     model.eval()
+    name=0
     with torch.no_grad():
         for (img, label) in test_dl:
-            img, label = img.to(device), label.to(device)
-            pred = model(img)
-            img, label, pred = img.squeeze(0), label.squeeze(0), pred.squeeze(0)
-            pred2 = torch.zeros(2, 512, 512).to(device)
-            index = torch.cat([torch.zeros(1,512, 512), torch.ones(1,512, 512)], dim=0).long().to(device)
-            pred2.scatter_(0, index, torch.cat([1-pred, pred], dim=0))
+            imgs, labels = img.to(device), label.to(device)
+            preds = model(img)
+            name += 1
+            for i in range(args.batch_size):
+                img, label, pred = imgs[i,:],labels[i,:],preds[i,:]
 
-            U = pred2.cpu().numpy().transpose(2,0,1).reshape((2,-1)) # [2, 512*512]
-            U = np.where(a )
-            U = -np.log(U)
-            d.setUnaryEnergy(U)
-            print(U)
-            im = np.array(img.cpu(), dtype=np.uint8).transpose(1, 2, 0)
-            pairwise_energy = create_pairwise_bilateral(sdims=(10, 10), schan=(0.01,), img=im, chdim=2)
-            d.addPairwiseEnergy(pairwise_energy, compat=10)
+                probs = torch.stack([1-pred, pred], dim=0).cpu().numpy()
+                img, label  = img.cpu().numpy(), label.cpu().numpy()
+                pairwise_energy = create_pairwise_bilateral(sdims=(10,10), schan=(0.01,), img=img, chdim=0)
+                U = unary_from_softmax(probs)
+                d = dcrf.DenseCRF2D(512, 512, 2)
+                d.setUnaryEnergy(U)
+                d.addPairwiseEnergy(pairwise_energy, compat=10)
 
-            Q = d.inference(5)
-            map = np.argmax(Q, axis=0).reshape((512, 512))
-            proba = np.array(Q)
-            print(proba)
+                Q = d.inference(100)
+                map = np.argmax(Q, axis=0).reshape((512, 512))
+                print(map.shape)
 
-            fig, ax = plt.subplots(1, 3, figsize=(15, 5))
-            plt.subplots_adjust(top=1, bottom=0, hspace=0.01)
-            ax[0].imshow(img.cpu().numpy().transpose(1,2,0), cmap='gray')
-            ax[1].imshow(label.cpu().numpy().transpose(1,2,0), cmap='gray')
-            ax[2].imshow(map, cmap='gray')
+                img = (255. / img.max() * (img - img.min())).astype(np.uint8)
+                label = (255. / label.max() * (label - label.min())).astype(np.uint8)
+                pred = (255. / map.max() * (map - map.min())).astype(np.uint8)
 
-            plt.show()
+                img = Image.fromarray(img[0,:], mode='L')
+                label = Image.fromarray(label[0,:], mode='L')
+                pred = Image.fromarray(pred, mode='L')
 
-            break
+                img.save(f'./results/{name}_{i}_i.png')
+                label.save(f'./results/{name}_{i}_l.png')
+                pred.save(f'./results/{name}_{i}_p.png')
+
 
 
 
